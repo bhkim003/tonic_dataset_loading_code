37d9949d850b7aec1704e1e6833ab7f7eb606849
BH lab <bhkim003@naver.com>
2025-06-14 01:31:01 +0900

    HI

---
+++ tonic_custom/datasets/dvsgesture.py
@@ -0,0 +1,206 @@
+import os
+from typing import Callable, Optional
+import numpy as np
+import torch
+from tonic.dataset import Dataset
+
+
+class DVSGesture(Dataset):
+    """`IBM DVS Gestures <http://research.ibm.com/dvsgesture/>`_
+
+    .. note::  This is (exceptionally) a preprocessed version of the original dataset,
+               where recordings that originally contained multiple labels have already
+               been cut into respective samples. Also temporal precision is reduced to ms.
+
+    ::
+
+        @inproceedings{amir2017low,
+          title={A low power, fully event-based gesture recognition system},
+          author={Amir, Arnon and Taba, Brian and Berg, David and Melano, Timothy and McKinstry, Jeffrey and Di Nolfo, Carmelo and Nayak, Tapan and Andreopoulos, Alexander and Garreau, Guillaume and Mendoza, Marcela and others},
+          booktitle={Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition},
+          pages={7243--7252},
+          year={2017}
+        }
+
+    Parameters:
+        save_to (string): Location to save files to on disk.
+        train (bool): If True, uses training subset, otherwise testing subset.
+        transform (callable, optional): A callable of transforms to apply to the data.
+        target_transform (callable, optional): A callable of transforms to apply to the targets/labels.
+        transforms (callable, optional): A callable of transforms that is applied to both data and
+                                         labels at the same time.
+    """
+
+    test_url = "https://figshare.com/ndownloader/files/38020584"
+    train_url = "https://figshare.com/ndownloader/files/38022171"
+    test_md5 = "56070e45dadaa85fff82e0fbfbc06de5"
+    train_md5 = "3a8f0d4120a166bac7591f77409cb105"
+    test_filename = "ibmGestureTest.tar.gz"
+    train_filename = "ibmGestureTrain.tar.gz"
+    classes = [
+        "hand_clapping",
+        "right_hand_wave",
+        "left_hand_wave",
+        "right_arm_clockwise",
+        "right_arm_counter_clockwise",
+        "left_arm_clockwise",
+        "left_arm_counter_clockwise",
+        "arm_roll",
+        "air_drums",
+        "air_guitar",
+        "other_gestures",
+    ]
+
+    sensor_size = (128, 128, 2)
+    dtype = np.dtype([("x", np.int16), ("y", np.int16), ("p", bool), ("t", np.int64)])
+    ordering = dtype.names
+
+    def __init__(
+        self,
+        save_to: str,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+        clipping: int = 1,
+        time: int = 8,
+        exclude_class: bool = False,
+        crop_max_time: int = 600_000,
+    ):
+        super().__init__(
+            save_to,
+            transform=transform,
+            target_transform=target_transform,
+            transforms=transforms,
+        )
+        self.train = train
+        if train:
+            self.url = self.train_url
+            self.file_md5 = self.train_md5
+            self.filename = self.train_filename
+            self.folder_name = "ibmGestureTrain"
+        else:
+            self.url = self.test_url
+            self.file_md5 = self.test_md5
+            self.filename = self.test_filename
+            self.folder_name = "ibmGestureTest"
+
+        if not self._check_exists():
+            self.download()
+
+        self.exclude_class = exclude_class
+        self.crop_max_time = crop_max_time
+
+        file_path = os.path.join(self.location_on_system, self.folder_name)
+        data_num = 0
+        for path, dirs, files in os.walk(file_path):
+            dirs.sort()
+            for file in files:
+                if file.endswith("npy"):
+                    this_file_max_time = np.load(path + "/" + file)[-1][3] * 1000 # us단위
+                    if self.crop_max_time >= this_file_max_time:  # 시간 길이 짧으면 스킵
+                        continue
+                    if self.exclude_class == True and int(file[:-4]) == 10: ## other class 제거
+                        continue
+                    data_num += 1
+                    self.data.append(path + "/" + file)
+                    self.targets.append(int(file[:-4]))
+        print("이 데이터셋의 데이터 개수는", data_num, "입니다. (test set은 그대로입니다.)")
+        self.clipping = clipping
+        self.time = time
+
+
+        # minimum_length = 9999999999999
+        # print('data개수', len(self.data))
+        # for i in range(len(self.data)):
+        #     x = np.load(self.data[i])
+        #     time = x[-1, 3]
+        #     if time < minimum_length:
+        #         minimum_length = time
+        # print('minimum_length', minimum_length)
+
+        # data개수 979
+        # minimum_length 2456.344
+        # data개수 240
+        # minimum_length 1798.364
+
+
+        # last_spike_time_box = []
+        # for i in range(len(self.data)):
+        #     last_spike_time_box.append(np.load(self.data[i])[-1][-1]*0.001)
+
+        # # 분포 히스토그램 그리기
+        # import matplotlib.pyplot as plt
+        # plt.figure(figsize=(10, 6))
+        # plt.hist(last_spike_time_box, bins=20, color='skyblue', edgecolor='black')
+        # plt.title(f"Distribution of Final Spike Timesteps (Total samples: {len(self.data)})")
+        # plt.xlabel("Final Timestep")
+        # plt.ylabel("Frequency")
+
+        # # max, min, 평균 계산 및 timestep 단위 환산
+        # max_val = np.max(last_spike_time_box)
+        # min_val = np.min(last_spike_time_box)
+        # mean_val = np.mean(last_spike_time_box)
+
+        # # 0.025로 나눈 값
+        # max_ts = max_val/ 0.025
+        # min_ts = min_val/ 0.025
+        # mean_ts = mean_val/ 0.025
+
+        # # 통계선 표시
+        # plt.axvline(max_val, color='red', linestyle='dashed', linewidth=1.5,
+        #             label=f"Max: {max_val} ({max_ts:.2f} timesteps)")
+        # plt.axvline(min_val, color='green', linestyle='dashed', linewidth=1.5,
+        #             label=f"Min: {min_val} ({min_ts:.2f} timesteps)")
+        # plt.axvline(mean_val, color='orange', linestyle='dashed', linewidth=1.5,
+        #             label=f"Mean: {mean_val:.2f} ({mean_ts:.2f} timesteps)")
+
+        # plt.legend()
+        # plt.grid(True)
+        # plt.tight_layout()
+        # plt.show()
+
+    def __getitem__(self, index):
+        """
+        Returns:
+            a tuple of (events, target) where target is the index of the target class.
+        """
+        events = np.load(self.data[index])
+        events[:, 3] *= 1000  # convert from ms to us
+        events = np.lib.recfunctions.unstructured_to_structured(events, self.dtype)
+        target = self.targets[index]
+        if self.transform is not None:
+            events = self.transform(events)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        if self.transforms is not None:
+            events, target = self.transforms(events, target)
+
+        ## BH code ###############################################
+        T, *spatial_dims = events.shape
+        if T >= self.time:
+            events = events[:self.time]
+        else:
+            assert False, f'self.time: {self.time}, T: {T}, events.shape: {events.shape}, index: {index}, self.data[index]: {self.data[index]}'
+            return self.__getitem__(random.randint(0, len(self.data) - 1))
+            
+            # pad_shape = (self.time - T, *spatial_dims)
+            # padding = np.zeros(pad_shape, dtype=events.dtype)
+            # events = np.concatenate([events, padding], axis=0)
+
+        if self.clipping != 0:
+            events[events<self.clipping] = 0.0
+            events[events>=self.clipping] = 1.0
+        events = torch.tensor(events, dtype=torch.float32)
+        ## BH code ###############################################
+            
+        return events, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def _check_exists(self):
+        return (
+            self._is_file_present()
+            and self._folder_contains_at_least_n_files_of_type(100, ".npy")
+        )
---
+++ tonic_dvs_gesture_setting.py
@@ -0,0 +1,318 @@
+## 알아서 데이터를 웹에서 다운로드 하게 돼 있습니다. 따로 웹에서 데이터를 안 가져오셔도 됩니다.
+
+## 아래 패키지들 설치 필요합니다.
+
+## 아마 hashlib 말고는 다 설치가 돼 있을 것 같습니다.
+## tonic_custom은 이 작업 폴더에 있으니까 설치 안하셔도 됩니다.
+
+## 일단 그냥 RUN 해보시면 바로 되게 해놨습니다.
+
+import os
+import torch
+import matplotlib.pyplot as plt
+import hashlib
+
+import tonic_custom as tonic
+from tonic_custom import DiskCachedDataset
+
+def data_loader(which_data, data_path, BATCH, IMAGE_SIZE, TIME, dvs_clipping, dvs_duration, exclude_class, merge_polarities, 
+                denoise_on, my_seed, extra_train_dataset, num_workers, chaching_on, pin_memory):
+
+    ### DVS_GESTURE_TONIC ########################################################################################################
+    ### DVS_GESTURE_TONIC ########################################################################################################
+    ### DVS_GESTURE_TONIC ########################################################################################################
+    ### DVS_GESTURE_TONIC ########################################################################################################
+    if (which_data == 'DVS_GESTURE_TONIC'):
+        data_dir = data_path
+        
+        train_dataset_list = []
+        # extra_train_dataset 이거 개수만큼 train dataset 더 가져옵니다.
+        for extra_train_index in range(extra_train_dataset+1):
+            # classes = [
+            #     "hand_clapping",
+            #     "right_hand_wave",
+            #     "left_hand_wave",
+            #     "right_arm_clockwise",
+            #     "right_arm_counter_clockwise",
+            #     "left_arm_clockwise",
+            #     "left_arm_counter_clockwise",
+            #     "arm_roll",
+            #     "air_drums",
+            #     "air_guitar",
+            #     "other_gestures",
+            # ]
+            train_compose = []
+
+            # polarity 없애기
+            if merge_polarities == True:
+                train_compose.append(tonic.transforms.MergePolarities()) 
+            
+            # 샘플 짜르기
+            crop_max_time = 100_000 + extra_train_index*dvs_duration*(TIME+1) + dvs_duration*(TIME+1) + 100_000
+            crop_min_time = 100_000 + extra_train_index*dvs_duration*(TIME+1)
+            train_compose.append(tonic.transforms.CropTime(max=crop_max_time))
+            train_compose.append(tonic.transforms.CropTime(min=crop_min_time))
+
+            # denoise 하기
+            if denoise_on == True:
+                train_compose.append(tonic.transforms.Denoise(filter_time=10_000)) # 10_000 # 낮을수록 더 많이 거름
+            
+            # 이미지 크기 조정
+            train_compose.append(tonic.transforms.Downsample(spatial_factor=IMAGE_SIZE/tonic.datasets.DVSGesture.sensor_size[0]))
+            train_compose.append(tonic.transforms.ToFrame(
+                sensor_size=(IMAGE_SIZE,IMAGE_SIZE,2),
+                time_window=dvs_duration, 
+                include_incomplete=False))
+            
+            train_transform = tonic.transforms.Compose(train_compose)
+
+            # test dataset #####################################################################################################
+            # test dataset은 extra_train_index에 상관없이 항상 동일한 crop_max_time, crop_min_time을 사용합니다.
+            # 따라서 for문 밖으로 빼도 되는데 밑에 caching에 문제 생길까봐 그냥 놔뒀습니다.
+            test_compose = []
+            if merge_polarities == True:
+                test_compose.append(tonic.transforms.MergePolarities()) #polarity 없애기
+            crop_max_time_test = 100_000 + dvs_duration*(TIME+1)
+            crop_min_time_test = 100_000
+            test_compose.append(tonic.transforms.CropTime(max=crop_max_time_test))
+            test_compose.append(tonic.transforms.CropTime(min=crop_min_time_test))
+            if denoise_on == True:
+                test_compose.append(tonic.transforms.Denoise(filter_time=10_000)) # 10_000 # 낮을수록 더 많이 거름
+            test_compose.append(tonic.transforms.Downsample(spatial_factor=IMAGE_SIZE/tonic.datasets.DVSGesture.sensor_size[0]))
+            test_compose.append(tonic.transforms.ToFrame(
+                sensor_size=(IMAGE_SIZE,IMAGE_SIZE,2),
+                time_window=dvs_duration, 
+                include_incomplete=False))
+            test_transform = tonic.transforms.Compose(test_compose)
+            # test dataset #####################################################################################################
+
+
+            train_dataset_temp = tonic.datasets.DVSGesture(data_dir, train=True, transform=train_transform, clipping = dvs_clipping, time = TIME, exclude_class = exclude_class, crop_max_time = crop_max_time)
+            test_dataset = tonic.datasets.DVSGesture(data_dir, train=False, transform=test_transform, clipping = dvs_clipping, time = TIME, exclude_class = exclude_class, crop_max_time = crop_max_time_test)
+
+            ## disk에 dataset caching하기 ###################################################################
+            train_transform_settings = {
+                'train_transform': train_transform,
+                'test_transform': test_transform,
+                'clipping': dvs_clipping,
+                'time': TIME,
+                'exclude_class': exclude_class,
+            }
+            settings_str = str(train_transform_settings)
+            dataset_hash = hashlib.md5(settings_str.encode()).hexdigest()
+            print(f'dataset_hash = {dataset_hash}')
+
+            # cache 폴더가 없으면 생성
+            os.makedirs(f"{data_dir}/DVSGesture/cache/", exist_ok=True)
+
+            my_cache_path = f"{data_dir}/DVSGesture/cache/fast_dataloading_{dataset_hash}"
+            if os.path.exists(my_cache_path):
+                print('cache path exists')
+            else:
+                print('cache path doesn\'t exist')
+                os.mkdir(my_cache_path)
+                os.mkdir(f"{my_cache_path}/train")
+                os.mkdir(f"{my_cache_path}/test")
+
+            if chaching_on == True:
+                train_dataset_list.append(DiskCachedDataset(train_dataset_temp, cache_path=f"{my_cache_path}/train"))
+                test_dataset = DiskCachedDataset(test_dataset, cache_path=f"{my_cache_path}/test")
+            else: 
+                train_dataset_list.append(train_dataset_temp)
+                ################################################################################################
+        
+        train_dataset = torch.utils.data.ConcatDataset(train_dataset_list)
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH, shuffle = True, num_workers=num_workers, drop_last=False, generator=torch.Generator().manual_seed(my_seed), pin_memory = pin_memory)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH, shuffle = False, num_workers=num_workers, drop_last=False, pin_memory = pin_memory)
+        
+        in_channel = 1 if merge_polarities == True else 2
+        CLASS_NUM = 10 if exclude_class == True else 11
+
+        train_data_count = len(train_dataset) 
+    
+    ### NMNIST_TONIC ########################################################################################################
+    ### NMNIST_TONIC ########################################################################################################
+    ### NMNIST_TONIC ########################################################################################################
+    ### NMNIST_TONIC ########################################################################################################
+    elif (which_data == 'NMNIST_TONIC'):
+        data_dir = data_path 
+        assert extra_train_dataset == 0, 'NMNIST는 extra_train_dataset을 지원하지 않습니다.'
+        
+        compose = []
+
+        # polarity 없애기
+        if merge_polarities == True:
+            compose.append(tonic.transforms.MergePolarities()) #polarity 없애기
+
+        # 샘플 짜르기
+        compose.append(tonic.transforms.CropTime(max=(10_000 + dvs_duration*(TIME+1))+100_000))
+        compose.append(tonic.transforms.CropTime(min=(10_000)))
+
+        # denoise 하기
+        if denoise_on == True:
+            compose.append(tonic.transforms.Denoise(filter_time=10_000)) # 10_000 # 낮을수록 더 많이 거름
+
+        # 이미지 크기 조정
+        compose.append(tonic.transforms.Downsample(spatial_factor=IMAGE_SIZE/tonic.datasets.NMNIST.sensor_size[0]))
+        compose.append(tonic.transforms.ToFrame(
+            sensor_size=(IMAGE_SIZE,IMAGE_SIZE,2),
+            time_window=dvs_duration, 
+            include_incomplete=False))
+        
+        transform = tonic.transforms.Compose(compose)
+
+        train_dataset = tonic.datasets.NMNIST(data_dir, train=True, transform=transform, clipping = dvs_clipping, time = TIME)
+        test_dataset = tonic.datasets.NMNIST(data_dir, train=False, transform=transform, clipping = dvs_clipping, time = TIME)
+        
+
+        ## disk에 dataset caching하기 ###################################################################
+        transform_settings = {
+            'train_transform': transform,
+            'test_transform': transform,
+            'clipping': dvs_clipping,
+            'time': TIME
+        }
+        settings_str = str(transform_settings)
+        dataset_hash = hashlib.md5(settings_str.encode()).hexdigest()
+        print(f'dataset_hash = {dataset_hash}')
+
+        # cache 폴더가 없으면 생성
+        os.makedirs(f"{data_dir}/DVSGesture/cache/", exist_ok=True)
+        
+        my_cache_path = f"{data_dir}/NMNIST/cache/fast_dataloading_{dataset_hash}"
+        if os.path.exists(my_cache_path):
+            print('cache path exists')
+        else:
+            print('cache path doesn\'t exist')
+            os.mkdir(my_cache_path)
+            os.mkdir(f"{my_cache_path}/train")
+            os.mkdir(f"{my_cache_path}/test")
+
+        if chaching_on == True:
+            train_dataset = DiskCachedDataset(train_dataset, cache_path=f"{my_cache_path}/train")
+            test_dataset = DiskCachedDataset(test_dataset, cache_path=f"{my_cache_path}/test")
+        else:
+            train_dataset = train_dataset
+            test_dataset = test_dataset
+        ################################################################################################
+
+
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH, shuffle = True, num_workers=num_workers, drop_last=False, generator=torch.Generator().manual_seed(my_seed), pin_memory = pin_memory)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH, shuffle = False, num_workers=num_workers, drop_last=False, pin_memory = pin_memory)
+
+        in_channel = 1 if merge_polarities == True else 2
+        CLASS_NUM = 10
+        train_data_count = len(train_dataset) 
+
+    else:
+        assert False, 'which_data가 잘못됨.'
+
+    return train_loader, test_loader, in_channel, CLASS_NUM, train_data_count
+
+print('\n')
+
+train_loader, test_loader, synapse_conv_in_channels, CLASS_NUM, train_data_count = data_loader(
+        which_data = 'DVS_GESTURE_TONIC', 
+        # 'DVS_GESTURE_TONIC' or 'NMNIST_TONIC' 
+
+        data_path = '/data2', 
+        # '/data2' or '/data3', etc. # gpu 서버 경로 설정. nfs 로 해놓지만 않으면 됩니다.
+
+        BATCH = 16, 
+        # 아무거나
+
+        IMAGE_SIZE = 128, 
+        # 아무거나 # DVS_GESTURE_TONIC 128, # NMNIST_TONIC 34가 원본이긴 합니다만 원하는 사이즈 하셔도 됩니다. 저는 14로 했습니다.
+        # 다만 down_sizing하면 down_pooling 된 스파이크를 모두 더합니다. max_pool이 아니고 sum_pool(?)입니다.
+
+        TIME = 8, 
+        # 아무거나
+
+        dvs_clipping = 1,
+        # 0 이상 아무거나
+        # 이 숫자보다 크거나 같을 때 해당 칸을 1, 작으면 0으로 clpping 됩니다. dvs_duration 시간 동안 스파이크 개수가 누적되기도하고
+        # IMAGE_SIZE를 작게하면 sum_pooling 되어 누적되기도 하니까 이 dvs_clipping을 1이상으로 설정해야 됩니다.
+        # 만약 dvs_clipping을 0으로 하면 clipping은 하지않고 모든 스파이크를 다 더한 정수값이 나옵니다.  
+
+        dvs_duration = 250_000,
+        # 아무거나 
+
+        exclude_class = True,
+        # DVS-Gesture의 11번째 클래스를 배제합니다. NMNIST와는 무관합니다.
+
+        merge_polarities = True,
+        # True로 하면 polarity를 합칩니다. 즉 negative polarity와 positive polarity의 스파이크를 합쳐서 첫번째 채널에 둡니다.
+        # 두번째 채널의 값은 모두 0으로 둡니다. 채널 차원 자체는 2로 유지됩니다.!
+
+        denoise_on = False,
+        # True로 하면 denoise가 적용됩니다. caching 때문에 처음 한 epoch만 오래 걸리고 그 다음부터는 빠릅니다.
+
+        my_seed = 1,
+        # seed 지정입니다. train_loader 셔플 시드 값입니다.
+
+        extra_train_dataset = 0,
+        # train dataset에서 남은 타임 동안에서 데이터를 더 가져옵니다. 즉, 샘플개수가 늘어나는 효과가 있습니다.
+        # test_dataset에는 적용되지 않습니다.
+        # DVS_GESTURE_TONIC에서만 사용됩니다.
+
+        num_workers = 2,
+        # 2 추천
+        # 저는 그냥 2로 합니다. 이걸 해야 무슨 CPU(GPU아님)를 덜 먹었었나 그랬습니다.
+
+        chaching_on = True,
+        # True 추천
+        # 이걸 해야 데이터 캐싱이 가능합니다.
+
+        pin_memory = True,
+        # True 추천
+        # 저는 그냥 2로 합니다. 이걸 해야 무슨 CPU(GPU아님)를 덜 먹었었나 그랬습니다.
+        
+        ) 
+
+print('\n')
+
+
+for inputs, targets in train_loader:
+    
+    print("inputs.shape :", inputs.shape, '[batch, time, channel, height, width]')   # 전체 배치 크기
+
+    print("targets.shape:", targets.shape, '[batch]')  # 라벨 shape
+    print("1st sample shape:", inputs[0].shape, '[time, channel, height, width]')  # 배치에서 첫 번째 샘플
+
+    img0 = inputs[0][0][0].cpu().numpy()  # 첫 번째 이미지
+    img1 = inputs[0][0][1].cpu().numpy()  # 두 번째 이미지
+
+
+    plt.figure(figsize=(8, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img0, cmap='gray')
+    plt.title("inputs[x][x][0]")
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(img1, cmap='gray')
+    plt.title("inputs[x][x][1]")
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig("snapshot.png")  # 현재 작업 디렉토리에 저장
+    # plt.show()  # 저장만 하고 시각화는 생략하려면 주석 처리
+    print('\n')
+    print('MergePolarities가 적용된 경우, inputs[x][x][0]은 양극성의 합입니다.')
+    print('MergePolarities가 적용되었다고 해서 channel 차원이 1이 되는 것은 아니고,\n그냥 두번째 채널의 모든 값이 0이 되는 것입니다.')
+
+
+
+    break  # 한 번만 보기 위해 break
+
+### ./tonic_custom/datasets/dvsgesture.py
+### 위 파일 코드의 class DVSGesture(Dataset) 를 보시면 
+### 제가 좀 커스텀 해놓았습니다. 여기서 dvs_clipping과 DVS-Gesture 11번째 클래스를 배제하는 exclude_class 진행합니다.
+
+# MergePolarities가 적용된 경우, inputs[x][x][0]은 양극성의 합입니다.
+# MergePolarities가 적용되었다고 해서 channel 차원이 1이 되는 것은 아니고,
+# 그냥 두번째 채널의 모든 값이 0이 되는 것입니다.
+
